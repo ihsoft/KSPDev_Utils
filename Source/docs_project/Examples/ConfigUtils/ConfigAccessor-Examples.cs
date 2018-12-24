9f32136719b52a917f1b19b62c35d5dfae77fb53
ihsoft <igor.zavoychinskiy@gmail.com>
2018-12-23 21:38:20 -0800

    Migrate from shared repository

---
+++ Source/docs_project/Examples/ConfigUtils/ConfigAccessor-Examples.cs
@@ -0,0 +1,25 @@
+﻿// Kerbal Development tools - Examples
+// Author: igor.zavoychinskiy@gmail.com
+// This software is distributed under Public domain license.
+
+using KSPDev.ConfigUtils;
+
+namespace ConfigUtils.Examples {
+
+#region ReadPartConfigExample
+public class ReadPartConfigExample : PartModule {
+  public override void OnAwake() {
+    base.OnAwake();
+    // get hthe custom fields from the prefab.
+    ConfigAccessor.CopyPartConfigFromPrefab(this);
+  }
+
+  public override void OnLoad(ConfigNode node) {
+    base.OnLoad(node);
+    // Load part's config custom fields in the scenes that clone the parts.
+    ConfigAccessor.ReadPartConfig(this, node);
+  }
+}
+#endregion
+
+}  // namespace
