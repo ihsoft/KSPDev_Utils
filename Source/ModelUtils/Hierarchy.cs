0f7dc75986ee97c4f7b0dbf9679968ee5076bc20
ihsoft <igor.zavoychinskiy@gmail.com>
2019-04-01 19:14:09 -0700

    Add safe destory method

--- Source/ModelUtils/Hierarchy.cs
+++ Source/ModelUtils/Hierarchy.cs
@@ -306,6 +306,26 @@ public static class Hierarchy {
     return unescapedName.Replace("/", "//");
   }
 
+  /// <summary>Destroys the object in a way which is safe for physical callback methods.</summary>
+  /// <remarks>
+  /// The Unity <c>UnityEngine.Object.Destroy</c> method only marks object for deletion, but before
+  /// the next fixed frame cycle completed, the object still can be found in the hierarchy. And it
+  /// may trigger physics before the final cleanup. This method ensures that none of these
+  /// side-effects happen and it <i>doesn't</i> use physics incompatible <c>DestroyImmediate</c>
+  /// method.
+  /// </remarks>
+  public static void SafeDestory(Transform obj) {
+    SafeDestory(obj.gameObject);
+  }
+
+  /// <inheritdoc cref="SafeDestory(Transform)"/>
+  public static void SafeDestory(GameObject obj) {
+    obj.transform.parent = null;
+    obj.name = "$disposed";
+    obj.SetActive(false);
+    UnityEngine.Object.Destroy(obj);
+  }
+
   #region Local helper methods.
   static void GatherHirerachyNames(Transform parent, string pathPrefix, List<string> names) {
     var fullName = pathPrefix != ""
