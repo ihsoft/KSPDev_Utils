4ff127ee0e7cfeb3a3e5f1000cadea08fcad7ddd
ihsoft <igor.zavoychinskiy@gmail.com>
2019-10-18 00:44:49 -0700

    Add UIScalableWindowController

---
+++ Source/PrefabUtils/UIScalableWindowController.cs
@@ -0,0 +1,57 @@
+﻿// Kerbal Development tools for Unity scripts.
+// Author: igor.zavoychinskiy@gmail.com
+// This software is distributed under Public domain license.
+
+using KSP.UI;
+using KSPDev.LogUtils;
+using KSPDev.KSPInterfaces;
+using System;
+using UnityEngine;
+
+namespace KSPDev.PrefabUtils {
+
+/// <summary>Base script that reacts to the game UI scale changes.</summary>
+/// <remarks>
+/// This component needs to be added to a game object to have effect. The primary targets of this
+/// component are the dynamic prefabs. On scale change, the GUI object will be repositioned
+/// according to its anchors. A message <c>ControlUpdated</c> will be broadcasted to the components
+/// on the parent object to let them updating as needed. 
+/// </remarks>
+public class UIScalableWindowController : MonoBehaviour, IsDestroyable {
+  #region Local fields and properties
+  /// <summary>Previously known scale onf UI.</summary>
+  /// <seealso cref="OnGameSettingsApplied"/>
+  protected float previousUiScale { get; private set; }
+  #endregion
+
+  #region MonoBehaviour overrides
+  /// <inheritdoc/>
+  public virtual void Awake() {
+    previousUiScale = UIMasterController.Instance.uiScale;
+    GameEvents.OnGameSettingsApplied.Add(OnGameSettingsApplied);
+  }
+
+  /// <inheritdoc/>
+  public virtual void OnDestroy() {
+    GameEvents.OnGameSettingsApplied.Remove(OnGameSettingsApplied);
+  }
+  #endregion
+
+  #region Local utility methods
+  /// <summary>Callback that is called when game settings, like UI scale, are changed.</summary>
+  protected virtual void OnGameSettingsApplied() {
+    var scaleDelta = UIMasterController.Instance.uiScale - previousUiScale;
+    DebugEx.Info("Game settings changed: scaleDelta={0}, window={1}", scaleDelta, gameObject.name);
+    previousUiScale = UIMasterController.Instance.uiScale;
+    var mainRect = transform as RectTransform;
+    if (mainRect != null) {
+      mainRect.anchoredPosition *= 1f - scaleDelta;
+      SendMessage("ControlUpdated", gameObject, SendMessageOptions.DontRequireReceiver);
+    } else {
+      DebugEx.Error("No RectTransform found!");
+    }
+  }
+  #endregion
+}
+
+}  // namespace
